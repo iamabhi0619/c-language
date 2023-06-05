484c2cb905c51b13af4d40d123ca02c0a17a11ab
Abhishek Kumar <131546128+iamabhi0619@users.noreply.github.com>
2023-06-05 20:03:51 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,7 @@
+# include<stdio.h>
+
+int main()
+{
+    printf("Hello World"); // printing Hello World
+    return 0;
+}
---
+++ Input_Output.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age= ");
+    scanf("%d", &age); //input
+    printf("Age entered by you is %d",age); //output
+    return 0;
+}
