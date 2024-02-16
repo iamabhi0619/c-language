72e24afa10b76d2d59b03ebab49ce8365e9399d4
iamabhi0619 <iamabhi0619@gmail.com>
2024-02-16 20:31:03 +0530

    adding comments

--- New_Code/pattern_1.c
+++ New_Code/pattern_1.c
@@ -3,10 +3,12 @@ int main(){
     int n, a=1;
     printf("Enter the number of row:- ");
     scanf("%d",&n);
-    for(int i=0; i<=n; i++){
-        a = i;
+    //loop for number of rows
+    for(int i=1; i<=n; i++){
+        a = i;//updating the value of a to row number
+        //loop for column
         for(int j=0; j<i; j++){
-            printf("%d\t",a);
+            printf("%d\t",a);//printing the numbers
             a++;
         }
         printf("\n");
--- New_Code/pattern_2.c
+++ New_Code/pattern_2.c
@@ -3,16 +3,24 @@ int main(){
     int n, a=1;
     printf("Enter the number of row:- ");
     scanf("%d",&n);
-    for(int i=0; i<=n; i++){
+    //loop for number of rows
+    for(int i=1; i<=n; i++){
         a = i;
+        //loop for column
         for(int j=0; j<n-i; j++){
-            printf(" \t");
+            printf(" \t");//printing the blank spaces
         }
         for(int j=0; j<i; j++){
-            printf("%d\t",a);
+            printf("%d\t",a);//print the rows
             a++;
         }
         printf("\n");
     }
     return 0;
 }
+
+//output
+//                         1
+//                 2       3
+//         3       4       5
+// 4       5       6       7
